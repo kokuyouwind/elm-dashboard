7ac87a4f2a77dc611450d0e80b4852581a22b58f
kokuyouwind <kokuyouwind@gmail.com>
2018-01-25 23:28:00 +0900

    add Ui.Container layout

--- src/App.elm
+++ src/App.elm
@@ -1,15 +1,30 @@
 module App exposing (main)
 
 import Html exposing (div)
+import Html.Attributes exposing (style)
 import Markdown
-
+import Ui.Container
 
 main =
-  div [] [
-    Markdown.toHtml [] markdown1,
-    Markdown.toHtml [] markdown2
-  ]
+  Ui.Container.view
+    { direction = "column", align = "center", compact = True }
+    [ style [ ("margin", "10px auto"), ("width", "80%") ] ]
+    [ Ui.Container.view
+        { direction = "row", align = "start", compact = False }
+        [ style [ ("width", "100%") ] ]
+        [ Markdown.toHtml [ markdownStyle ] markdownTop ],
+      Ui.Container.view
+        { direction = "row", align = "start", compact = False }
+        [ style [ ("margin-top", "10px"), ("width", "100%") ] ]
+        [ Markdown.toHtml [ markdownStyle ] markdown1,
+          Markdown.toHtml [ markdownStyle ] markdown2]
+    ]
 
+markdownTop = """
+# This is Top Markdown
+
+test
+"""
 
 markdown1 = """
 
@@ -28,3 +43,5 @@ markdown2 = """
 test
 
 """
+
+markdownStyle = style [ ("border", "solid"), ("padding", "10px"), ("width", "100%") ]
