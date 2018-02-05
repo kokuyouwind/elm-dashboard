4b65bdc6ef43009828e18ca5a784507083060ecd
kokuyouwind <kokuyouwind@gmail.com>
2018-02-05 22:45:58 +0900

    layout by elm-mdl

--- src/App.elm
+++ src/App.elm
@@ -1,47 +1,68 @@
 module App exposing (main)
 
-import Html exposing (div)
-import Html.Attributes exposing (style)
+import Html exposing (..)
 import Markdown
-import Ui.Container
+import Material
+import Material.Color as Color
+import Material.Scheme as Scheme
+import Material.Card as Card
+import Material.Options as Options exposing (css)
+import Material.Elevation as Elevation
+import Material.Grid exposing (grid, cell, size, Device(..))
 
+main : Program Never Model Msg
 main =
-  Ui.Container.view
-    { direction = "column", align = "center", compact = True }
-    [ style [ ("margin", "10px auto"), ("width", "80%") ] ]
-    [ Ui.Container.view
-        { direction = "row", align = "start", compact = False }
-        [ style [ ("width", "100%") ] ]
-        [ Markdown.toHtml [ markdownStyle ] markdownTop ],
-      Ui.Container.view
-        { direction = "row", align = "start", compact = False }
-        [ style [ ("margin-top", "10px"), ("width", "100%") ] ]
-        [ Markdown.toHtml [ markdownStyle ] markdown1,
-          Markdown.toHtml [ markdownStyle ] markdown2]
+  program
+    { init = (model, Cmd.none)
+    , update = update
+    , view = view
+    , subscriptions = always Sub.none }
+
+type alias Model = { mdl : Material.Model }
+
+model : Model
+model = { mdl = Material.model }
+
+type Msg = Mdl (Material.Msg Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    Mdl msg_ -> Material.update Mdl msg_ model
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ Scheme.topWithScheme Color.Teal Color.Red grids
     ]
 
-markdownTop = """
-# This is Top Markdown
+grids =
+  grid []
+    [ cell [ size All 6 ] [ card ]
+    , cell [ size All 6 ] [ card ]
+    , cell [ size All 4 ] [ card ]
+    , cell [ size All 4 ] [ card ]
+    , cell [ size All 4 ] [ card ]
+    , cell [ size All 3 ] [ card ]
+    , cell [ size All 3 ] [ card ]
+    , cell [ size All 3 ] [ card ]
+    , cell [ size All 3 ] [ card ]
+    ]
 
-test
+card = Card.view
+  [ Elevation.e2
+  , css "width" "100%" ]
+  [ Card.text [] [ Markdown.toHtml [] markdown ]
+  ]
+
+markdown = """
+# This is Title
+## This is Header
+### This is SubHeader
+
+test strings
+
+ * test
+ * test
+ * test
 """
-
-markdown1 = """
-
-# This is Markdown 1
-
- * item
- * item
- * item
-
-"""
-
-markdown2 = """
-
-# This is Markdown 2
-
-test
-
-"""
-
-markdownStyle = style [ ("border", "solid"), ("padding", "10px"), ("width", "100%") ]
